e41274e581588c6cf98a3515dfa72a3ab0c5e99b
Nyxterius <Nyxterius@proton.me>
2025-12-16 16:54:51 -0500

    Created Project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const grid = require('gridjs');
+const bodyParser = require('body-parser');
+const supabaseClient = require('@supabase/supabase-js');
+const dotenv = require('dotenv');
+
+const app = express();
+
+const port = 3000;
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'))
+
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
+
+app.get('/', (req, res) => {
+  res.sendFile('public/homepage.html', { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
